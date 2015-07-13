676fd749b0b9cdf9e88765f2eaa396bbab1e7b53
TEAM42kr <hudsucker@naver.com>
2015-07-13 20:39:50 +0900

    로그인기능구현

--- BidulgiCommon/src/com/teamnexters/bidulgi/common/request/BidulgiRequestUri.java
+++ BidulgiCommon/src/com/teamnexters/bidulgi/common/request/BidulgiRequestUri.java
@@ -5,4 +5,5 @@ public interface BidulgiRequestUri {
 	public String REQUEST_LOGIN = "/login";
 	public String REQUEST_LOGIN_SUCCESS = "/service/login-success";
 	public String REQUEST_REQUIRE_LOGIN = "/require-login";
+	public String REQUEST_LOGIN_FAIL = "/login-fail";
 }
--- BidulgiCommon/src/com/teamnexters/bidulgi/common/response/BidulgiResponseCode.java
+++ BidulgiCommon/src/com/teamnexters/bidulgi/common/response/BidulgiResponseCode.java
@@ -4,4 +4,5 @@ public interface BidulgiResponseCode {
 	public int RESPONSE_TEST = 0;
 	public int RESPONSE_REQUIRE_LOGIN  = 1;
 	public int RESPONSE_LOGIN_SUCCESS = 2;
+	public int RESPONSE_LOGIN_FAIL = 3;
 }
