64c22da3781c04654e30aaf3b787c63433d38e41
TEAM42kr <hudsucker@naver.com>
2015-07-14 09:38:04 +0900

    no message

--- BidulgiCommon/src/com/teamnexters/bidulgi/common/request/BidulgiRequestUri.java
+++ BidulgiCommon/src/com/teamnexters/bidulgi/common/request/BidulgiRequestUri.java
@@ -6,5 +6,4 @@ public interface BidulgiRequestUri {
 	public String REQUEST_LOGIN_SUCCESS = "/service/login-success";
 	public String REQUEST_LOGIN_FAIL = "/login-fail";
 	public String REQUEST_REQUIRE_LOGIN = "/require-login";
-	public String REQUEST_LOGIN_FAIL = "/login-fail";
 }
