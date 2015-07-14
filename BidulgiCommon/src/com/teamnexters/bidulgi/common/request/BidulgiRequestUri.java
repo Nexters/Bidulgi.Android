85397cd7c47e935e5bf7847cb643d11287ad7cbf
TEAM42kr <hudsucker@naver.com>
2015-07-14 09:25:04 +0900

    로그인기능 구현

--- BidulgiCommon/src/com/teamnexters/bidulgi/common/request/BidulgiRequestUri.java
+++ BidulgiCommon/src/com/teamnexters/bidulgi/common/request/BidulgiRequestUri.java
@@ -4,5 +4,6 @@ public interface BidulgiRequestUri {
 	public String REQUEST_TEST = "/test";
 	public String REQUEST_LOGIN = "/login";
 	public String REQUEST_LOGIN_SUCCESS = "/service/login-success";
+	public String REQUEST_LOGIN_FAIL = "/login-fail";
 	public String REQUEST_REQUIRE_LOGIN = "/require-login";
 }
