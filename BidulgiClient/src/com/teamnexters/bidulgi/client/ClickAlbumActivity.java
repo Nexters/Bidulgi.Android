f97d71204e1eb7ccfdb8d88e7d3be5a586b96351
Yangsiyoung <Yangsiyoung@YangSiYoung>
2016-01-02 17:08:20 +0900

    사진 클릭헀을때 저장 할 수 있는 메뉴 추가

--- BidulgiClient/src/com/teamnexters/bidulgi/client/ClickAlbumActivity.java
+++ BidulgiClient/src/com/teamnexters/bidulgi/client/ClickAlbumActivity.java
@@ -5,7 +5,8 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 import com.bumptech.glide.Glide;
-
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -15,18 +16,31 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ClickAlbumActivity extends Activity {
 
 	Intent intent;
 	private ImageView imgTraining;
 	private Bitmap bitmap;
+	private ActionBar actionBar;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_click_album);
+
+		actionBar = getActionBar();
+		actionBar.setTitle(null);
+		actionBar.setIcon(null);
+		actionBar.setDisplayHomeAsUpEnabled(false);
+		actionBar.setDisplayUseLogoEnabled(false);
+		actionBar.setHomeButtonEnabled(false);
 
 		intent = getIntent();
 		imgTraining = (ImageView) findViewById(R.id.imgTraining);
@@ -34,46 +48,79 @@ public class ClickAlbumActivity extends Activity {
 		Glide.with(getApplicationContext()).load(intent.getExtras().getString("imgTrainingURL")).into(imgTraining);
 
 		Log.d("aaaa", "외부 저장소 절대 경로는 " + Environment.getExternalStorageDirectory().toString());
-		try {
-			final File path = new File(Environment.getExternalStorageDirectory().toString() + "/Pictures/bidoolgi/");
-			if (!path.isDirectory()) {
-				path.mkdir();
-			}
-			new AsyncTask<Void, Void, Void>() {
 
-				@Override
-				protected Void doInBackground(Void... params) {
-					// TODO Auto-generated method stub
-					try {
-						bitmap = Glide.with(getApplicationContext())
-								.load(intent.getExtras().getString("imgTrainingURL")).asBitmap().into(100, 100).get();
-						// Drawable drawable = imgTraining.getDrawable();
-						// bitmap = ((BitmapDrawable)drawable).getBitmap();
-					} catch (Exception e) {
-						Log.d("error", "글라이드로 비트맵 변환 시 에러 내용은 " + e.toString());
-					}
-					return null;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.click_albumpicture, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+
+		switch (item.getItemId()) {
+		
+		case R.id.itemExit:
+			finish();
+			break;
+			
+
+		case R.id.itemDownload:
+
+			try {
+				final File path = new File(
+						Environment.getExternalStorageDirectory().toString() + "/Pictures/bidoolgi/");
+				if (!path.isDirectory()) {
+					path.mkdir();
 				}
+				new AsyncTask<Void, Void, Void>() {
 
-				protected void onPostExecute(Void result) {
-					try {
-						File file = new File(path.getAbsolutePath(), "bidoolgi.jpg");
-						OutputStream outputStream = new FileOutputStream(file);
-
-						bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-						outputStream.flush();
-						outputStream.close();
-					} catch (Exception e) {
-						Log.d("error", "비트맵 저장 시 에러내용은 " + e.toString());
+					@Override
+					protected Void doInBackground(Void... params) {
+						// TODO Auto-generated method stub
+						try {
+							bitmap = Glide.with(getApplicationContext())
+									.load(intent.getExtras().getString("imgTrainingURL")).asBitmap().into(100, 100)
+									.get();
+							// Drawable drawable = imgTraining.getDrawable();
+							// bitmap = ((BitmapDrawable)drawable).getBitmap();
+						} catch (Exception e) {
+							Log.d("error", "글라이드로 비트맵 변환 시 에러 내용은 " + e.toString());
+						}
+						return null;
 					}
-				};
-			}.execute();
 
-		} catch (Exception e) {
-			Log.d("error", "프로필 사진 보기에서 Glide를 통해 bitmap을 추출하는 도중 오류 내용은 " + e.toString());
+					protected void onPostExecute(Void result) {
+						try {
+							File file = new File(path.getAbsolutePath(), "bidoolgi.jpg");
+							OutputStream outputStream = new FileOutputStream(file);
 
+							bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+							outputStream.flush();
+							outputStream.close();
+							Toast.makeText(getApplicationContext(), "사진 저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
+						} catch (Exception e) {
+							Log.d("error", "비트맵 저장 시 에러내용은 " + e.toString());
+						}
+					};
+				}.execute();
+
+			} catch (Exception e) {
+				Log.d("error", "프로필 사진 보기에서 Glide를 통해 bitmap을 추출하는 도중 오류 내용은 " + e.toString());
+
+			}
+
+			break;
+
+		
 		}
-
+		return super.onOptionsItemSelected(item);
 	}
 
 }
--- BidulgiClient/src/com/teamnexters/bidulgi/client/ClickProfileActivity.java
+++ BidulgiClient/src/com/teamnexters/bidulgi/client/ClickProfileActivity.java
@@ -8,23 +8,40 @@ import java.io.OutputStream;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
-
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ClickProfileActivity extends Activity {
 
 	private Intent intent;
 	private ImageView imgProfile;
+	private ActionBar actionBar;
+	private Bitmap bitmap;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_click_profile);
+
+		actionBar = getActionBar();
+		actionBar.setTitle(null);
+		actionBar.setIcon(null);
+		actionBar.setDisplayHomeAsUpEnabled(false);
+		actionBar.setDisplayUseLogoEnabled(false);
+		actionBar.setHomeButtonEnabled(false);
 
 		intent = getIntent();
 		imgProfile = (ImageView) findViewById(R.id.imgProfile);
@@ -34,21 +51,96 @@ public class ClickProfileActivity extends Activity {
 		} else {
 			Glide.with(getApplicationContext()).load(intent.getExtras().getString("imgProfileURL")).into(imgProfile);
 		}
-		/*try{
-			File path = new File("/sdcard/image/bidoolgi/");
-			if(!path.isDirectory()){
-			path.mkdir();
+		/*
+		 * try{ File path = new File("/sdcard/image/bidoolgi/");
+		 * if(!path.isDirectory()){ path.mkdir(); } File file = new
+		 * File(path.getAbsolutePath(), "bidoolgi.PNG"); OutputStream
+		 * outputStream = new FileOutputStream(file); Bitmap bitmap =
+		 * Glide.with(getApplicationContext()).load(intent.getExtras().getString
+		 * ("imgProfileURL")).asBitmap().into(100,100).get();
+		 * bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+		 * outputStream.flush(); outputStream.close();
+		 * 
+		 * }catch(Exception e){ Log.d("error",
+		 * "프로필 사진 보기에서 Glide를 통해 bitmap을 추출하는 도중 오류 내용은 " + e.toString()); }
+		 */
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.click_albumpicture, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+
+		switch (item.getItemId()) {
+
+		case R.id.itemExit:
+			finish();
+			break;
+
+		case R.id.itemDownload:
+
+			if (intent.getExtras().getString("imgProfileURL") == null) {
+				Toast.makeText(getApplicationContext(), "프로필 사진이 없습니다.", Toast.LENGTH_SHORT).show();
+
+			} else {
+				try {
+					final File path = new File(
+							Environment.getExternalStorageDirectory().toString() + "/Pictures/bidoolgi/");
+					if (!path.isDirectory()) {
+						path.mkdir();
+					}
+					new AsyncTask<Void, Void, Void>() {
+
+						@Override
+						protected Void doInBackground(Void... params) {
+							// TODO Auto-generated method stub
+							try {
+								bitmap = Glide.with(getApplicationContext())
+										.load(intent.getExtras().getString("imgTrainingURL")).asBitmap().into(100, 100)
+										.get();
+								// Drawable drawable =
+								// imgTraining.getDrawable();
+								// bitmap =
+								// ((BitmapDrawable)drawable).getBitmap();
+							} catch (Exception e) {
+								Log.d("error", "글라이드로 비트맵 변환 시 에러 내용은 " + e.toString());
+							}
+							return null;
+						}
+
+						protected void onPostExecute(Void result) {
+							try {
+								File file = new File(path.getAbsolutePath(), "bidoolgi.jpg");
+								OutputStream outputStream = new FileOutputStream(file);
+
+								bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+								outputStream.flush();
+								outputStream.close();
+								Toast.makeText(getApplicationContext(), "사진 저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
+							} catch (Exception e) {
+								Log.d("error", "비트맵 저장 시 에러내용은 " + e.toString());
+							}
+						};
+					}.execute();
+
+				} catch (Exception e) {
+					Log.d("error", "프로필 사진 보기에서 Glide를 통해 bitmap을 추출하는 도중 오류 내용은 " + e.toString());
+
+				}
+
+				break;
+
 			}
-			File file = new File(path.getAbsolutePath(), "bidoolgi.PNG");
-			OutputStream outputStream = new FileOutputStream(file);
-		Bitmap bitmap = Glide.with(getApplicationContext()).load(intent.getExtras().getString("imgProfileURL")).asBitmap().into(100,100).get();
-		bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-		outputStream.flush();
-		outputStream.close();
-		
-		}catch(Exception e){
-			Log.d("error", "프로필 사진 보기에서 Glide를 통해 bitmap을 추출하는 도중 오류 내용은 " + e.toString());
-		}*/
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
